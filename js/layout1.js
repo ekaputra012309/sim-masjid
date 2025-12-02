54ca437e3c73107bf95fb24e4893c40a7e4e0fa7
ekaputra012309 <eko012309@gmail.com>
2025-12-02 16:09:57 +0700

    add 2 layout and sidebar

---
+++ js/layout1.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  // Load partials
+  $("#header").load("partials/header.html", function () {
+    if (pb.authStore.model) {
+      $("#usernameDisplay").text(pb.authStore.model.name);
+    }
+  });
+
+  $("#sidebarContainer").load("partials/sidebar.html", function () {
+    // Toggle sidebar for mobile
+    $(document).on("click", "#toggleSidebar", function () {
+      $("#sidebar").addClass("show");
+    });
+
+    // Close sidebar (FIXED)
+    $(document).on("click", "#closeSidebar", function () {
+      $("#sidebar").removeClass("show");
+    });
+  });
+
+  $("#footer").load("partials/footer.html");
+});
